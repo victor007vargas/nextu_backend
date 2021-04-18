693214f22ad2d6bdeab32d510898d2ef446140d5
Programador 1 <vvargas2000@gmail.com>
2021-04-18 12:21:24 -0500

    Fase5 : Logica de Busquedas

--- src/public/js/actions.js
+++ src/public/js/actions.js
@@ -7,29 +7,8 @@ document.getElementById('buscar').onclick=bajardatos /* ***** accion del boton *
 
 function bajardatos(){
 
-  alert("Se dio clic en bajar todos los datos ");
-  $.ajax ({/* ***** bajar lista inmuebles *******/
-  url:'data1.json',
-  type:'GET',
-  data:{},
-  success: function (data){
-  alert(" Se bajaron los datos");
-  lis = data.slice()
   mostrartod()
-  }
-})
 
-
-$.ajax ({/* ***** bajar imagenes *******/
-  url:'data2.json',
-  type:'GET',
-  data:{},
-  success: function (data){
-  alert(" Se bajaron las imagenes");
-  limg = data.slice()
-  mostrartod()
-  } 
-})
 }
 /* ***** asignacion imagen *******/
 function preparetod(){
@@ -54,6 +33,35 @@ for (var i=0 ;i<=100 ;i++){
   $(".panel").append('<div class="bloque1"> <li> <div class="cajon"> <div class="bloque1a"> <img src="'+imgpredio+'" /> </div><div class="bloque1b"> <div class="direccion"> Direccion  : '+lis[i].Direccion+' </div> <div class="ciudad">     Ciudad : '+lis[i].Ciudad+'  </div> <div class="telefono">   Teléfono : '+lis[i].Telefono+'  </div><div class="codigop">    Codigo Postal :'+lis[i].Codigo_Postal+'   </div><div class="tipo">       Tipo : '+lis[i].Tipo+'  </div><div class="precio">     Precio : <div class="prenum">  '+lis[i].Precio+'    </div>  </div></div></div> </li></div>')
   
 }
+}
 
+/** □□□□□□□□□□□□□□□□□□ 2. Acceso a los datos □□□□□□□□□□□□□□□□□□**/
+function accesdata () {
+  
+  $.ajax ({/* ***** bajar lista inmuebles *******/
+    url:'data1.json',
+    type:'GET',
+    data:{},
+    success: function (data){
+    lis = data.slice()
+    
+    }
+  })
+  
+  
+  $.ajax ({/* ***** bajar imagenes *******/
+    url:'data2.json',
+    type:'GET',
+    data:{},
+    success: function (data){
+    limg = data.slice()
+   
+    } 
+  })
 
 }
+
+accesdata ()
+
+/** □□□□□□□□□□□□□□□□□□ 3.  Logica para las busquedas □□□□□□□□□□□□□□□□□□*/
+/** **** 3.1. Creacion de Lista de Ciudades *****/
